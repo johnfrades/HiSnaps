c28203575e92563e903d91aed09d8209afc8d481
User <User>
2016-09-06 15:55:23 +1200

    add comment, edit user, aggregation

--- test1.js
+++ test1.js
@@ -2,10 +2,16 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var moment = require("moment");
+var passport = require("passport");
+var passportLocal = require("passport-local");
+var methodOverride = require("method-override");
+
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost/userProfiles3");
 var mongoosedb = mongoose.connection
@@ -14,12 +20,32 @@ mongoosedb.once('open', function(){
 	console.log("Connected to database!");
 });
 
+
 var testSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	comments: [
+		{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Comment"
+		}
+	]
+});
+
+var commentSchema = new mongoose.Schema ({
+	name: String,
+	text: String,
+	date: String
 });
 
 var TestData = mongoose.model("User", testSchema);
+var Comment = mongoose.model("Comment", commentSchema);
+
+var nowDateAndTime = moment().format("l , LT")
+
+
+
+
 
 //GET ROUTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -36,6 +62,15 @@ app.get("/new", function(req, res) {
 	res.render("new");
 });
 
+app.get("/index/:id/edit", function(req, res) {
+	TestData.findById(req.params.id, function(err, editUser){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("editProfile", {editUser: editUser});
+		}
+	});
+});
 
 //SHOW pictures
 app.get("/index", function(req, res){
@@ -48,13 +83,46 @@ app.get("/index", function(req, res){
 	});
 });
 
-//SHOW more info
+
+//SHOW more info and comments
 app.get("/index/:id", function(req,res){
-	TestData.findById(req.params.id, function(err, userID){
+	TestData.findById(req.params.id).populate("comments").exec(function(err, userID){
 		if(err) {
 			console.log(err);
 		} else {
 			res.render("profile", {userID: userID});
+		}
+	});
+});
+
+app.post("/index/:id/comments", function(req, res){
+	
+	TestData.findById(req.params.id, function(err, foundUser){
+		if (err) {
+			console.log(err);
+		} else {
+			Comment.create(req.body.comment, function(err, theComment){
+				console.log(req.body.comment);
+				if (err) {
+					console.log(err);
+				} else {
+					theComment.save();
+					foundUser.comments.push(theComment);
+					foundUser.save();
+					console.log(theComment);
+					res.redirect("/index/" + foundUser._id);
+				}
+			});
+		}
+	});
+});
+
+app.get("/index/:id/comments/new", function(req, res){
+	TestData.findById(req.params.id, function(err, userID){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("newComment", {userID: userID, nowDateAndTime: nowDateAndTime});
 		}
 	});
 });
@@ -82,6 +150,23 @@ app.post("/index", function(req, res){
 	});
 
 });
+
+
+
+//EDIT route
+
+app.put("/index/:id", function(req, res){
+	TestData.findByIdAndUpdate(req.params.id, req.body.user, function(err, updateUser){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(updateUser);
+			res.redirect("/index/" + updateUser._id);
+		}
+	})
+})
+
+
 
 
 
